864e741874953f74bc4654eb3ee8f2794c669aad
Markus Raufer <markus.raufer@gmail.com>
2023-05-11 19:43:03 +0200

    refactor(EdgeSet): remove lazy implementation

--- Foundation.Graph/EdgeSet.cs
+++ Foundation.Graph/EdgeSet.cs
@@ -9,11 +9,11 @@ public class EdgeSet<TNode, TEdge> : IEdgeSet<TNode, TEdge>
 {
     public event NotifyCollectionChangedEventHandler? CollectionChanged;
 
-    private readonly Lazy<ICollection<TEdge>> _edges;
+    private readonly ICollection<TEdge> _edges;
 
     public EdgeSet()
     {
-        _edges = new Lazy<ICollection<TEdge>>(() => new List<TEdge>());
+        _edges = new List<TEdge>();
     }
     public EdgeSet([DisallowNull] IEnumerable<TEdge> edges) : this(edges.ToList())
     {
@@ -21,19 +21,17 @@ public class EdgeSet<TNode, TEdge> : IEdgeSet<TNode, TEdge>
 
     public EdgeSet([DisallowNull] ICollection<TEdge> edges)
     {
-        if (null == edges) throw new ArgumentNullException(nameof(edges));
-
-        _edges = new Lazy<ICollection<TEdge>>(() => edges);
+        _edges = edges.ThrowIfNull();
     }
 
     public void AddEdge([DisallowNull] TEdge edge)
     {
         ArgumentNullException.ThrowIfNull(edge, nameof(edge));
 
-        if (!AllowDuplicateEdges && _edges.Value.Contains(edge))
+        if (!AllowDuplicateEdges && _edges.Contains(edge))
             throw new EdgeSetException("edge exists");
 
-        _edges.Value.Add(edge);
+        _edges.Add(edge);
         
         CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, edge));
     }
@@ -44,10 +42,10 @@ public class EdgeSet<TNode, TEdge> : IEdgeSet<TNode, TEdge>
 
         foreach (var edge in edges)
         {
-            if (!AllowDuplicateEdges && _edges.Value.Contains(edge))
+            if (!AllowDuplicateEdges && _edges.Contains(edge))
                 throw new EdgeSetException("edge exists");
             
-            _edges.Value.Add(edge);
+            _edges.Add(edge);
         }
 
         CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, edges.ToArray()));
@@ -60,19 +58,19 @@ public class EdgeSet<TNode, TEdge> : IEdgeSet<TNode, TEdge>
 
     public void ClearEdges()
     {
-        _edges.Value.Clear();
+        _edges.Clear();
         CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
     }
 
-    public int EdgeCount => _edges.Value.Count;
+    public int EdgeCount => _edges.Count;
 
-    public IEnumerable<TEdge> Edges => _edges.Value;
+    public IEnumerable<TEdge> Edges => _edges;
 
-    public bool ExistsEdge(TEdge edge) => _edges.Value.Contains(edge);
+    public bool ExistsEdge(TEdge edge) => _edges.Contains(edge);
 
     public bool ExistsEdge(TNode source, TNode target)
     {
-        return _edges.Value.Any(x => x.Source.Equals(source) && x.Target.Equals(target));
+        return _edges.Any(x => x.Source.Equals(source) && x.Target.Equals(target));
     }
 
     protected void FireCollectionChanged(object sender, NotifyCollectionChangedEventArgs args)
@@ -80,11 +78,13 @@ public class EdgeSet<TNode, TEdge> : IEdgeSet<TNode, TEdge>
         CollectionChanged?.Invoke(sender, args);
     }
 
-    public IEnumerable<TEdge> GetEdges([DisallowNull] TNode node) => _edges.Value.Where(e => e.Source.Equals(node) || e.Target.Equals(node));
+    public IEnumerable<TEdge> GetEdges([DisallowNull] TNode node)
+        => _edges.Where(e => e.Source.Equals(node) || e.Target.Equals(node));
 
     public bool RemoveEdge(TEdge edge)
     {
-        if (!_edges.Value.Remove(edge)) return false;
+        if (!_edges.Remove(edge)) return false;
+
         CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, edge));
         return true;
     }
@@ -94,7 +94,7 @@ public class EdgeSet<TNode, TEdge> : IEdgeSet<TNode, TEdge>
         var removedEdges = new List<TEdge>();
         foreach (var edge in edges)
         {
-            if (_edges.Value.Remove(edge)) removedEdges.Add(edge);
+            if (_edges.Remove(edge)) removedEdges.Add(edge);
         }
 
         if (0 == removedEdges.Count) return;
@@ -110,23 +110,23 @@ public class EdgeSet<TNode, TEdgeId, TEdge> : IEdgeSet<TNode, TEdgeId, TEdge>
 {
     public event NotifyCollectionChangedEventHandler? CollectionChanged;
 
-    private readonly Lazy<IDictionary<TEdgeId, TEdge>> _edges = new (() => new Dictionary<TEdgeId, TEdge>());
+    private readonly IDictionary<TEdgeId, TEdge> _edges = new Dictionary<TEdgeId, TEdge>();
 
     /// <summary>
     /// Returns always false, because edges with same Id are not allowed.
     /// </summary>
     public bool AllowDuplicateEdges => false;
 
-    public int EdgeCount => _edges.Value.Count;
+    public int EdgeCount => _edges.Count;
 
-    public IEnumerable<TEdge> Edges => _edges.Value.Values;
+    public IEnumerable<TEdge> Edges => _edges.Values;
 
 
     public void AddEdge(TEdge edge)
     {
         ArgumentNullException.ThrowIfNull(edge, nameof(edge));
 
-        _edges.Value.Add(edge.Id, edge);
+        _edges.Add(edge.Id, edge);
         FireCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, edge));
     }
 
@@ -136,10 +136,10 @@ public class EdgeSet<TNode, TEdgeId, TEdge> : IEdgeSet<TNode, TEdgeId, TEdge>
 
         foreach (var edge in edges)
         {
-            if (!AllowDuplicateEdges && _edges.Value.ContainsKey(edge.Id))
+            if (!AllowDuplicateEdges && _edges.ContainsKey(edge.Id))
                 throw new EdgeSetException("edge exists");
 
-            _edges.Value.Add(edge.Id, edge);
+            _edges.Add(edge.Id, edge);
         }
 
         CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, edges.ToArray()));
@@ -147,19 +147,19 @@ public class EdgeSet<TNode, TEdgeId, TEdge> : IEdgeSet<TNode, TEdgeId, TEdge>
 
     public void ClearEdges()
     {
-        if (0 == _edges.Value.Count) return;
+        if (0 == _edges.Count) return;
 
-        _edges.Value.Clear();
+        _edges.Clear();
         FireCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
     }
 
-    public bool ExistsEdge(TEdgeId edgeId) => _edges.Value.ContainsKey(edgeId);
+    public bool ExistsEdge(TEdgeId edgeId) => _edges.ContainsKey(edgeId);
 
-    public bool ExistsEdge(TEdge edge) => _edges.Value.Values.Any(e => e.Equals(edge));
+    public bool ExistsEdge(TEdge edge) => _edges.Values.Any(e => e.Equals(edge));
 
     public bool ExistsEdge(TNode source, TNode target)
     {
-        return _edges.Value.Values.Any(x => x.Source.Equals(source) && x.Target.Equals(target));
+        return _edges.Values.Any(x => x.Source.Equals(source) && x.Target.Equals(target));
     }
 
     protected void FireCollectionChanged(NotifyCollectionChangedEventArgs args)
@@ -169,17 +169,17 @@ public class EdgeSet<TNode, TEdgeId, TEdge> : IEdgeSet<TNode, TEdgeId, TEdge>
 
     public Option<TEdge> GetEdge(TEdgeId edgeId)
     {
-        if (_edges.Value.TryGetValue(edgeId, out TEdge? edge)) return Option.Some(edge);
+        if (_edges.TryGetValue(edgeId, out TEdge? edge)) return Option.Some(edge);
 
         return Option.None<TEdge>();
     }
 
-    public IEnumerable<TEdge> GetEdges([DisallowNull] TNode node) => _edges.Value.Values.Where(e => e.Source.Equals(node) || e.Target.Equals(node));
+    public IEnumerable<TEdge> GetEdges([DisallowNull] TNode node) => _edges.Values.Where(e => e.Source.Equals(node) || e.Target.Equals(node));
 
 
     public bool RemoveEdge([DisallowNull] TEdge edge)
     {
-        var removed = _edges.Value.Remove(edge.Id);
+        var removed = _edges.Remove(edge.Id);
         if (removed) FireCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, edge));
 
         return removed;
@@ -195,7 +195,7 @@ public class EdgeSet<TNode, TEdgeId, TEdge> : IEdgeSet<TNode, TEdgeId, TEdge>
             return RemoveEdge(optionalEdge.OrThrow());
         }
 
-        return _edges.Value.Remove(edgeId);
+        return _edges.Remove(edgeId);
     }
 
     public void RemoveEdges(IEnumerable<TEdgeId> edgeIds)
@@ -209,7 +209,7 @@ public class EdgeSet<TNode, TEdgeId, TEdge> : IEdgeSet<TNode, TEdgeId, TEdge>
         var removedEdges = new List<TEdge>();
         foreach (var edge in edges)
         {
-            if (_edges.Value.Remove(edge.Id)) removedEdges.Add(edge);
+            if (_edges.Remove(edge.Id)) removedEdges.Add(edge);
         }
 
         if (0 == removedEdges.Count) return;
